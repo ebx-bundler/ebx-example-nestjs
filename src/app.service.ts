930c8b07b4354528d8b6402545cd0e93275ca39f
Md Adil <md-adil@live.com>
2023-08-25 18:25:57 +0530

    adding decorators

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  database: string;
+  constructor(config: ConfigService) {
+    this.database = config.get('DATABASE', '');
+  }
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! ' + this.database;
   }
 }
